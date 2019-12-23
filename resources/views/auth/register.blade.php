8dfcb3fa561fd0e4b0ca1aafebf37d793c9d9b90
kartik k <akhramshaik@gmail.com>
2019-12-24 00:39:05 +0530

    login and register page designed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,59 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Algorithmica File Management</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="{{ asset('css/toastr.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/circle.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.5/angular.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-sortable/0.17.2/sortable.min.js"></script>
+    <!-- Angular Resources -->
+    <script src="{{ asset('js/angular/angular-file-upload.min.js')}}"></script>
+    <script src="{{ asset('js/angular/fileManager.js') }}"></script>
+</head>
+<body>
+<div id="app">
+
+
+<nav class="navbar navbar-inverse navbar-static-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a class="navbar-brand" href="{{ url('/') }}"> Algorithmica File Management </a>
+
+            </div>
+
+           
+        </div>
+    </nav>
+
+
+
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -73,4 +126,29 @@
         </div>
     </div>
 </div>
-@endsection
+
+
+
+
+</div>
+
+    <div style="margin-top: 25px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <span>
+                        
+                       <!--  <i class="fa fa-github"></i>&nbsp;&nbsp;<a target="_blank" href="https://github.com/morganlaneap/laravel-file-manager">Fork me on GitHub</a> -->
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Scripts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="{{ asset('js/toastr.min.js')}}"></script>
+<script src="{{ asset('js/app.js')}}"></script>
+</body>
+</html>
